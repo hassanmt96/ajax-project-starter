c66d655ec52bc1c8207fed8ad09bd9ef503326be
tamagri <tamagrijr@gmail.com>
2020-09-09 13:13:36 -0500

    error handeling

--- public/events.js
+++ public/events.js
@@ -1,43 +1,48 @@
 window.addEventListener('DOMContentLoaded', (e)=>{
-   
+
     fetch('/kitten/image')
         .then(res => {
+            console.log(res);
+            if(!res.ok){
+                throw Error(res);
+            }
             return res.json();
-
         })
         .then(data =>{
             let pic = document.querySelector('.cat-pic')
-            console.log(data)
+            // console.log(data)
             pic.src = data.src
         })
+        .catch(err => {
+            document.querySelector('.error').innerHTML ="Something went wrong! Please try again."
+            // alert("Something went wrong! Please try again.")
+        });
+
 document.getElementById('new-pic')
         .addEventListener('click', ()=>{
             document.querySelector('.loader').innerHTML= 'loading'
+            document.querySelector('.error').innerHTML =""
 
             fetch('/kitten/image')
             .then(res => {
+                if(!res.ok){
+                    throw Error(res);
+                }
                 return res.json();
-    
             })
             .then(data =>{
                 let pic = document.querySelector('.cat-pic')
-                console.log(data)
+                // console.log(data)
                 pic.src = data.src
                 document.querySelector('.loader').innerHTML= ''
-        })
-
-
-
-
+            })
+            .catch(err => {
+                document.querySelector('.error').innerHTML ="Something went wrong! Please try again."
+                // alert("Something went wrong! Please try again.")
+            });
     })
 
 
 
 
-
-
-
 })
-
-
-
